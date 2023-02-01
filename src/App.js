8629d42aa457eaa0c894413909a07cbd88540207
nischithamy25 <nischithamy58@gmail.com>
2023-02-01 11:28:50 -0800

    version2 update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import ExpenseItem from './components/ExpenseItem';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
+
 
 function App() {
   const expenses = [
     {id: 0,
       date: new Date (2021, 2, 21),
       title: 'Car Insurance',
-      amount: 260
+      amount: 261
     },
-    {id: 0,
+    {id: 1,
       date: new Date (2022, 3, 21),
       title: 'Paper Towel',
-      amount: 260
+      amount: 262
     },
-    {id: 0,
+    {id: 2,
         date: new Date (2023, 5, 2),
         title: 'Car Insurance',
-        amount: 260
+        amount: 263
     },
   ];
-  return (
+  /*return (
     <div>
       <p>welcome to react!!!!</p>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/> 
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/> 
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/> 
-      </div>
+     <Expenses item={Expenses}></Expenses>
+     </div>
+  );*/
+
+  return React.createElement(
+    'div',
+    {},
+    React.createElement("p",{}, "welcome to react!!"),
+    React.createElement(Expenses, { items: expenses })
   );
 }
 
